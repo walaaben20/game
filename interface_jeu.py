bed83c101af7dbe8982cb427c55a4ca67b80f2a2
walaaben20 <walabhy2022@gmail.com>
2024-03-11 14:40:07 +0100

    reglement de l'interface

--- interface_jeu.py
+++ interface_jeu.py
@@ -1,24 +1,41 @@
 import tkinter as tk
 
-class DevinetteGUI:
-    def __init__(self, master):
-        self.master = master
-        self.master.title("Jeu de Devinette")
+class JeuDevinette(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Jeu de Devinette")
+        self.geometry("400x200")
+        self.configure(bg='black')
 
-        self.label = tk.Label(master, text="Bienvenue dans le jeu Devinette !")
-        self.label.pack()
+        self.mot_a_deviner = ""
+        self.indice = 0
 
-        self.entry = tk.Entry(master)
-        self.entry.pack()
+        self.label1 = tk.Label(self, text="Joueur 1: Saisissez un mot à deviner :", fg='white', bg='black')
+        self.label1.pack()
 
-        self.button = tk.Button(master, text="Valider", command=self.check_guess)
+        self.entry1 = tk.Entry(self, show="*")
+        self.entry1.pack()
+
+        self.label2 = tk.Label(self, text="Joueur 2: Choisissez 1 pour commencer sans indice ou 2 pour avoir 3 indices", fg='white', bg='black')
+        self.label2.pack()
+
+        self.entry2 = tk.Entry(self)
+        self.entry2.pack()
+
+        self.button = tk.Button(self, text="Valider", command=self.start_game)
         self.button.pack()
 
-    def check_guess(self):
-        user_guess = self.entry.get()
-        # Ajoutez ici la logique pour vérifier la devinette et afficher le résultat
+    def start_game(self):
+        self.mot_a_deviner = self.entry1.get()
+        choix = int(self.entry2.get())
 
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = DevinetteGUI(root)
-    root.mainloop()
+        if choix == 1:
+            print("Le joueur 2 commence sans indice.")
+            # Ajoutez ici la logique pour permettre au joueur 2 de deviner sans indice
+        elif choix == 2:
+            print("Le joueur 2 a choisi d'avoir 3 indices.")
+            # Ajoutez ici la logique pour donner des indices au joueur 2 et lui permettre de deviner
+
+if __name__ == '__main__':
+    app = JeuDevinette()
+    app.mainloop()
