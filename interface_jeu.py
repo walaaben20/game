b6d3d7d7b3dd6d82332d1334d61e518dd68a1724
walaaben20 <walabhy2022@gmail.com>
2024-03-13 19:25:51 +0100

    suppression de quelque fichier et essayon de stylee le jeu

--- interface_jeu.py
+++ interface_jeu.py
@@ -3,6 +3,15 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+import os
+from kivy.config import Config
+
+
+
+os.environ["KIVY_EXTERNAL_CONFIG"] = "1"
+
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
 
 class DevinetteApp(App):
     def build(self):
